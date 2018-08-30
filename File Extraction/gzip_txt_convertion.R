f09467ab37cbea1d8f218d74360081e3df8ae645
Hasan <shabbir12hasan@gmail.com>
2018-08-30 21:53:37 +1000

    File extraction

---
+++ File Extraction/gzip_txt_convertion.R
@@ -0,0 +1,50 @@
+#####################################################
+# Some R code to get you going for the 
+# 2018 Melbourne Datathon
+#####################################################
+
+#install and load required libraries
+#install.packages('data.table')
+library(data.table)
+
+# stopping exponential values
+options(scipen = 999)
+
+#tell R where it can find the data
+ScanOnFolderMaster <- 'D:/StudyMaterial/DatathonMelb_2018/Samp_x/ScanOnTransaction'
+ScanOffFolderMaster <- 'D:/StudyMaterial/DatathonMelb_2018/Samp_x/ScanOffTransaction'
+
+
+# For running over all samples and converting them from gzip to txt.
+for (sample_folder in c(3:9)){
+  
+  #decide sample folder
+  mySamp <- sample_folder
+  
+  ScanOnFolder <- sub("x",mySamp,ScanOnFolderMaster)
+  ScanOffFolder <- sub("x",mySamp,ScanOffFolderMaster)
+  
+  #list the files
+  onFiles <- list.files(ScanOnFolder,recursive = TRUE,full.names = TRUE)
+  offFiles <- list.files(ScanOffFolder,recursive = TRUE,full.names = TRUE)
+  
+  #how many
+  allFiles <- union(onFiles,offFiles)
+  cat("\nthere are", length(allFiles),'files')
+  
+  # using gzip to unzip files
+  library(R.utils)
+  dt <- gunzip(myFile)
+  #dt <- unzip(myFile)
+  
+  
+  # code to unzip all files at once
+  # select offFiles or onFiles or both
+  # unzip all files
+  for (i in 1:157){
+    #myFile <- offFiles[i]
+    myFile <- onFiles[i]
+    dt <- gunzip(myFile)
+  }
+  
+}
